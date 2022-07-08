ad56053c5571aa3dc4309c5ef93f14eda70fd7ed
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-07-08 14:59:29 +0900

    add 最終

--- culture_property/settings.py
+++ culture_property/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 '''
+'''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "app/static")
 ]
-'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 
 # Default primary key field type
